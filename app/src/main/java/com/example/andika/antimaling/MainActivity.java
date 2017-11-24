7357b1dab95bc37461289336553607cce0bd25a6
andika yana <andikayana99@gmail.com>
2017-11-24 23:06:55 +0800

    Add Toggle & Music Alarm

--- app/src/main/java/com/example/andika/antimaling/MainActivity.java
+++ app/src/main/java/com/example/andika/antimaling/MainActivity.java
@@ -4,19 +4,36 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     Recriver recriver = new Recriver();
+    ToggleButton toggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
-        registerReceiver(recriver, filter);
+        toggleButton = findViewById(R.id.togle_btn);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked) {
+                    Toast.makeText(MainActivity.this, "Alarm Mulai", Toast.LENGTH_SHORT).show();
+                    IntentFilter filter = new IntentFilter();
+                    filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+                    registerReceiver(recriver, filter);
+                } else {
+                    unregisterReceiver(recriver);
+                }
+            }
+        });
 
     }
+
+
 }
--- app/src/main/java/com/example/andika/antimaling/Recriver.java
+++ app/src/main/java/com/example/andika/antimaling/Recriver.java
@@ -3,7 +3,11 @@ package com.example.andika.antimaling;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.widget.Toast;
+
+import java.io.IOException;
 
 /**
  * Created by andika on 24/11/17.
@@ -13,7 +17,20 @@ public class Recriver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Alarm Mulai", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "Alarm Bunyi", Toast.LENGTH_SHORT).show();
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        AssetFileDescriptor afd = null;
+        try {
+            afd = context.getAssets().openFd("siren.mp3");
+            mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+            afd.close();
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+
 }
